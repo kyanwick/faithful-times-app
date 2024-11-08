82629419962e12286db1b408fe3bee8ce5ee78c9
Kyan Oberas <kyanmigueloberas1@gmail.com>
2024-11-08 15:27:04 -0500

    Updates

---
+++ precache-manifest.211faee22445e1d9697ed17495239bcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075c924a0cd6b74bcf5b49f2c57fde19",
+    "url": "/faithful-times-app/index.html"
+  },
+  {
+    "revision": "36bc81b15ff14359bd3b",
+    "url": "/faithful-times-app/static/css/main.5f14db4b.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/faithful-times-app/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/faithful-times-app/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "36bc81b15ff14359bd3b",
+    "url": "/faithful-times-app/static/js/main.e4512c3e.chunk.js"
+  },
+  {
+    "revision": "ec508155480f29f8a117",
+    "url": "/faithful-times-app/static/js/runtime~main.7a7dedcd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e51ef2687b7f3584e8c56e3b5fb22ee.js"
+  "/faithful-times-app/precache-manifest.211faee22445e1d9697ed17495239bcf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/faithful-times-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
